f50f365686e75150684717d609c996aefed6cf77
Diego Vinícius <diego.cavalcante90@gmail.com>
2025-03-06 19:13:00 -0300

    fix: corrigindo bugs

--- src/br/com/treinaweb/main/Main.java
+++ src/br/com/treinaweb/main/Main.java
@@ -1,29 +1,51 @@
 package br.com.treinaweb.main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-/*        System.out.print("Digite uma mensagem: ");
-        String mensagem = scanner.next();
-        System.out.println(mensagem);*/
+        int acao = 1;
 
-        // int numero1 = Integer.parseInt(scanner.nextLine()); - Isso aqui é um Cast
-/*        System.out.print("Digite o primeiro número: ");
-        int numero1 = scanner.nextInt();
-        System.out.print("Digite a operação: ");
-        char operacao = scanner.next().charAt(0);
-        System.out.print("Digite o segundo número: ");
-        int numero2 = scanner.nextInt();
-        System.out.println(String.format("Você quer fazer a operação %d %c %d", numero1, operacao, numero2));*/
+        while (acao > 0) {
+            System.out.println("-------------- Operação Matemática Simples --------------");
 
-        String nome = scanner.nextLine();
-        int numero = scanner.nextInt();
-        System.out.println(nome);
-        System.out.println(numero);
+            System.out.print("Digite o primeiro número: ");
+            int numero1 = scanner.nextInt();
+            System.out.print("Digite a operação: ");
+            char operacao = scanner.next().charAt(0);
+            System.out.print("Digite o segundo número: ");
+            int numero2 = scanner.nextInt();
+            System.out.println(String.format("Você quer fazer a operação %d %c %d", numero1, operacao, numero2));
+            int resultado = 0;
+
+            switch (operacao) {
+                case '+':
+                    resultado = numero1 + numero2;
+                    break;
+                case '-':
+                    resultado = numero1 - numero2;
+                    break;
+                case '*':
+                    resultado = numero1 * numero2;
+                    break;
+                case '/':
+                    resultado = numero1 / numero2;
+                    break;
+                default:
+                    System.out.println("Operação inválida.");
+                    break;
+            }
+            ArrayList<String> historico = new ArrayList<String>();
+            String entradaDoHistorico = String.format("%d %c %d = %d", numero1, operacao, numero2, resultado);
+            historico.add(entradaDoHistorico);
+
+            System.out.println(entradaDoHistorico);
+
+        }
+
 
         scanner.close();
-
     }
 }
